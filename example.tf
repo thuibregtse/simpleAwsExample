38a56cfbf5a74e5e5e46f1af610fc92a4e49a81f
thuibregtse <tomhuib@gmail.com>
2020-05-22 13:51:09 -0600

    added a second machine
    2 VMs

--- example.tf
+++ example.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "machine2" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
