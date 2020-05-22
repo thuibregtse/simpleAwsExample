9d4648492ed3f63f682e6c7e8e02648e5db7c8e3
thuibregtse <tomhuib@gmail.com>
2020-05-22 12:26:15 -0600

    restore to original tutorial content
    same as in the article

--- example.tf
+++ example.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile    = "default"
-  region    = "us-west-2"
+  region     = "us-east-1"
 }
 
-resource "aws_instance" "example1a" {
-  ami           = "ami-0129cc68"
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
