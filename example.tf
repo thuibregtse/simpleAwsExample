64a065137748b7bd7248ad9fec1dc95745a9e74a
thuibregtse <tomhuib@gmail.com>
2020-05-22 14:17:28 -0600

    back to the original text
    This makes no sense...what I copied from the web seems to work, but the same config then says that the ami doesn't exist.

--- example.tf
+++ example.tf
@@ -4,11 +4,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
-}
-
-resource "aws_instance" "machine2" {
-  ami           = "ami-b374d5a5"
+  ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
