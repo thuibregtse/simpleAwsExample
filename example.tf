49d15942c45b3cd6f3a876f567a466b0f8473f98
thuibregtse <tomhuib@gmail.com>
2020-06-01 13:08:44 -0600

    different ami
    ami changed

--- example.tf
+++ example.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-west-1"
+  region     = "us-east-1e"
 }
 
-resource "aws_instance" "westExample" {  
+resource "aws_instance" "eastExample" {  
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 
